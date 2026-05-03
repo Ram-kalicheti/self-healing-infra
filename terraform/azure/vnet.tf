9628c69438e31f72621d8f52971da25dc76c6f12
Ram-kalicheti <kalichatiramz@gmail.com>
2026-05-02 23:33:35 -0400

    feat: add vnet with subnet and nsg

---
+++ terraform/azure/vnet.tf
@@ -0,0 +1,24 @@
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.cluster_name}-vnet"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "${var.cluster_name}-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.cluster_name}-nsg"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id                 = azurerm_subnet.aks.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
